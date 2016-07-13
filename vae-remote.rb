a955e6b53c275d66faaff54ee8dfe9b47ae33d48
Kevin Bombino <kevin@bombino.org>
2016-07-13 14:49:37 -0700

    Add Vae Remote

---
+++ vae-remote.rb
@@ -0,0 +1,8 @@
+class VaeRemote < Formula
+  desc "Vae Platform PHP Library that provides VaeML and Vae PHP functions."
+  url "https://github.com/actionverb/vae_remote/archive/1.0.0.tar.gz"
+  version "1.0.0"
+
+  def install
+  end
+end
