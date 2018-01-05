e8875b981d3b0d32ad24c5518c30eda9cd409525
Kevin Bombino <kevin@bombino.org>
2018-01-04 17:52:54 -0800

    updates served URL

--- served.rb
+++ served.rb
@@ -1,7 +1,7 @@
 class Served < Formula
   desc "https://github.com/datasift/served.git"
   homepage "https://github.com/datasift/served.git"
-  url "https://github.com/datasift/served/zipball/master/"
+  url "https://github.com/datasift/served/archive/6288e52c79c9ce72fe3dfadde1e0023b907acb3e.zip"
   version "1"
 
   def install
