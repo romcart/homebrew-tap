ce2bcb0399558286aedb03d5ab5e63e1b02b65a9
Kevin Bombino <kevin@bombino.org>
2018-01-04 17:49:14 -0800

    removes missing version

--- served.rb
+++ served.rb
@@ -2,6 +2,7 @@ class Served < Formula
   desc "https://github.com/datasift/served.git"
   homepage "https://github.com/datasift/served.git"
   url "https://github.com/datasift/served/zipball/master/"
+  version "1"
 
   def install
     system "cmake", "."
